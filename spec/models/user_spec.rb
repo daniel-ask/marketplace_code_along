689514e5bfbbbad1b05d2d6d1813b8789ccd68a9
Daniel <cdanielask@gmail.com>
2021-05-03 12:11:33 +1000

    MODEL: Added test and validations for the user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user){ 
+    described_class.new(
+      email: 'test@test.com',
+      password: 'password',
+      username: 'testuser'
+    )
+   }
+
+   it 'is valid with valid attributes' do
+    expect(user).to be_valid
+   end
+
+   describe 'validations' do
+    it 'should be invalid without a username' do
+      user.username = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should be invalid without a email' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should be invalid without a password' do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should be invalid with a password shorter than 6 characters' do
+      user.password = '1'
+      expect(user).to_not be_valid
+    end
+
+    it 'should be invalid with an email in the wrong format' do
+      user.email = 'test'
+      expect(user).to_not be_valid
+    end
+
+    it 'should check that username has only alphanumeric characters' do
+      user.username = 'd@$()'
+      expect(user).to_not be_valid
+    end
+
+    it 'should check that username has no spaces' do
+      user.username = 'd ok'
+      expect(user).to_not be_valid
+    end
+
+    it 'should check that emails are unique' do
+      
+    end
+    it 'should check that username is unique'
+    
+   end
+end
