f87258602c7313e4d4319bdc3f8c721a66158742
Daniel <cdanielask@gmail.com>
2021-05-17 17:07:38 +1000

    DATABASES: Added eager loading to profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,6 @@
 class ProfilesController < ApplicationController
   def dashboard
-    @profile = Profile.find(params[:id])
+    @profile = Profile.includes(user: { listings: [], purchases: [:listing]}).find(params[:id])
     @user = @profile.user
-    
   end
 end
